7dafd12bff1b6cdfeb5ccf744fc6ca69eb7832e9
len9-ph <75072493+len9-ph@users.noreply.github.com>
2021-11-14 18:42:41 +0300

    Delete Main.java

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Calculator calculator = new Calculator("7-10*43+sin(0.52)");
-        System.out.println(calculator.fromInfixToPostfix());
-    }
-}
